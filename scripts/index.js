385056bd8a8efef0531b87d56b9c59288c704adc
Dmitriy <dmitrii.asenenko@gmail.com>
2021-04-21 00:12:24 +0300

    hotfix: большой hotfix и рефакторинг

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,32 @@
 const profileEdit = document.querySelector('.profile__edit-button');
 const popupOverlay = document.querySelector('.overlay');
-const closePopupButton = document.querySelector('.popup__close');
+const closePopupButton = document.querySelector('.overlay__close');
 const profileUsername = document.querySelector('.profile__username');
 const profileDescription = document.querySelector('.profile__description');
-const formElement = document.querySelector('.popup__form');
-const nameInput = formElement.querySelector('.popup__username'); 
-const jobInput = formElement.querySelector('.popup__description'); 
+const formElement = document.querySelector('.form');
+const nameInput = formElement.querySelector('.form__username');
+const jobInput = formElement.querySelector('.form__description');
 
 
-function openPopup(e) {
+function openPopupHandler(e) {
     e.preventDefault();
     popupOverlay.classList.toggle('overlay_opened');
     nameInput.value = profileUsername.textContent;
     jobInput.value = profileDescription.textContent;
 }
 
-function closePopup(e) {
+function closePopupHandler(e) {
     e.preventDefault();
     popupOverlay.classList.toggle('overlay_opened');
 }
 
 function formSubmitHandler(e) {
-    e.preventDefault(); 
+    e.preventDefault();
     profileUsername.textContent = nameInput.value;
     profileDescription.textContent = jobInput.value;
-    closePopup(e);
+    closePopupHandler(e);
 }
 
-profileEdit.addEventListener('click', openPopup);
-closePopupButton.addEventListener('click', closePopup);
+profileEdit.addEventListener('click', openPopupHandler);
+closePopupButton.addEventListener('click', closePopupHandler);
 formElement.addEventListener('submit', formSubmitHandler);
